4c62895bae552a742def5fee7f0c2e16067bf99b
Crisan-Adrian <adriancatalincrisan@gmail.com>
2022-05-21 16:37:23 +0300

    Created AnimationEditorWindow

---
+++ Proiect Licenta/Assets/Editor/AnimationEditorWindow.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using Utilities;
+
+class AnimationEditorWindow : EditorWindow
+{
+    private string newAnimationButton = "Create New Animation";
+    private string prevStepButton = "Prev Step";
+    private string nextStepButton = "Next Step";
+    private string addStepButton = "Add Step";
+
+    private int currentStep;
+    private int stepsCount;
+
+    private AnimationEditor animationEditor;
+    private Animation animation;
+    private GameObject animationModel;
+
+    private GameObject currentModel;
+    private GameObject pastModel;
+
+    private List<string> keys = new List<string>();
+    private Dictionary<string, GameObject> _currentModelBodyParts = new Dictionary<string, GameObject>();
+    private Dictionary<string, GameObject> _pastModelBodyParts = new Dictionary<string, GameObject>();
+    private bool editModeEnabled;
+    private Vector2 scrollPos;
+    private AnimationStep defaultPose;
+
+    [MenuItem("Window/Animation Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(AnimationEditorWindow));
+    }
+
+    public void Awake()
+    {
+        animationEditor = new AnimationEditor();
+    }
+
+    private void OnEnable()
+    {
+        animationEditor = new AnimationEditor();
+        currentStep = 0;
+        stepsCount = 0;
+        animation = null;
+        Repaint();
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.Space();
+        ShowModelSelector();
+        ShowDefaultPoseSelector();
+        
+        bool editMode;
+        editMode = EditorGUILayout.BeginToggleGroup("Enable Editing", editModeEnabled);
+        if (editModeEnabled != editMode)
+        {
+            editModeEnabled = editMode;
+
+            SetActiveObjects();
+        }
+        
+        if (GUILayout.Button(newAnimationButton))
+        {
+            animationEditor.CreateNewAnimation();
+        }
+
+        ShowAnimationSelector();
+
+        if (animation != null)
+        {
+            ShowAnimationDetails();
+        }
+
+        EditorGUILayout.EndToggleGroup();
+    }
+
+    private void ShowDefaultPoseSelector()
+    {
+        defaultPose = (AnimationStep) EditorGUILayout.ObjectField("Default Pose:", defaultPose, typeof(AnimationStep), false);
+    }
+
+    private void SetActiveObjects()
+    {
+        GameObject model = GameObjectUtilities.FindChildWithName(animationModel, "Model");
+
+        model.SetActive(!editModeEnabled);
+        currentModel.SetActive(editModeEnabled);
+        pastModel.SetActive(editModeEnabled);
+    }
+
+    private void ShowAnimationDetails()
+    {
+        EditorGUILayout.Separator();
+
+        scrollPos =
+            EditorGUILayout.BeginScrollView(scrollPos);
+        EditorGUILayout.LabelField("Animation Details");
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUI.BeginDisabledGroup(currentStep == 0);
+        if (GUILayout.Button(prevStepButton))
+        {
+            Debug.Log(prevStepButton);
+            StepBackwards();
+        }
+        EditorGUI.EndDisabledGroup();
+
+        if (GUILayout.Button(addStepButton))
+        {
+            Debug.Log(addStepButton);
+        }
+
+        EditorGUI.BeginDisabledGroup(currentStep == stepsCount - 1);
+        if (GUILayout.Button(nextStepButton))
+        {
+            Debug.Log(nextStepButton);
+            StepForwards();
+        }
+
+        EditorGUI.EndDisabledGroup();
+        EditorGUILayout.EndHorizontal();
+
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(String.Format("Animation Steps:{0}", stepsCount));
+        EditorGUILayout.EndHorizontal();
+        currentStep = EditorGUILayout.IntField("Current Step:", currentStep, GUILayout.ExpandWidth(true));
+        
+        // Show current step inspector
+
+        Editor e = Editor.CreateEditor(animation.animationSteps[currentStep]);
+        
+        e.DrawDefaultInspector();
+        EditorGUILayout.EndScrollView();
+    }
+
+    private void StepForwards()
+    {
+        Animation animation = animationEditor.GetCurrentAnimation();
+        currentStep++;
+
+        int previousStep = currentStep - 1;
+
+        SetPastModelPose(animation.animationSteps[previousStep]);
+        if (currentStep < animation.animationSteps.Count)
+        {
+            SetCurrentModelPose(animation.animationSteps[currentStep]);
+        }
+
+        Repaint();
+    }
+
+    private void StepBackwards()
+    {
+        Animation animation = animationEditor.GetCurrentAnimation();
+
+        currentStep--;
+
+        int previousStep = currentStep - 1;
+
+        if (previousStep >= 0)
+        {
+            SetPastModelPose(animation.animationSteps[previousStep]);
+        }
+
+        SetCurrentModelPose(animation.animationSteps[currentStep]);
+        Repaint();
+    }
+
+    private void ShowModelSelector()
+    {
+        GameObject model = (GameObject) EditorGUILayout.ObjectField("Model:", animationModel, typeof(GameObject), true);
+        if (animationModel != model)
+        {
+            animationModel = model;
+            
+            currentModel = GameObjectUtilities.FindChildWithName(animationModel, "CurrentStep");
+            pastModel = GameObjectUtilities.FindChildWithName(animationModel, "PreviousStep");
+            
+            SetBodyParts(_currentModelBodyParts, currentModel);
+            SetBodyParts(_pastModelBodyParts, pastModel);
+        }
+    }
+
+    private void ShowAnimationSelector()
+    {
+        Animation a;
+        a = (Animation) EditorGUILayout.ObjectField("Current Animation:", animation, typeof(Animation), false);
+        if (a == null && animationModel != null && defaultPose != null)
+        {
+            SetPastModelPose(defaultPose);
+            SetCurrentModelPose(defaultPose);
+        }
+        if (animation != a)
+        {
+            animation = a;
+            animationEditor.SetAnimation(a);
+            currentStep = 0;
+            stepsCount = animation.animationSteps.Count;
+        }
+    }
+
+    private void SetBodyParts(Dictionary<string, GameObject> modelBodyParts, GameObject target)
+    {
+        modelBodyParts["body_lower"] = GameObjectUtilities.FindChildWithName(target, "Body_Lower");
+        modelBodyParts["body_upper"] = GameObjectUtilities.FindChildWithName(target, "Body_Upper");
+        modelBodyParts["left_leg_lower"] = GameObjectUtilities.FindChildWithName(target, "Left_Leg_Lower");
+        modelBodyParts["left_leg_upper"] = GameObjectUtilities.FindChildWithName(target, "Left_Leg_Upper");
+        modelBodyParts["right_leg_lower"] = GameObjectUtilities.FindChildWithName(target, "Right_Leg_Lower");
+        modelBodyParts["right_leg_upper"] = GameObjectUtilities.FindChildWithName(target, "Right_Leg_Upper");
+        modelBodyParts["left_arm_lower"] = GameObjectUtilities.FindChildWithName(target, "Left_Arm_Lower");
+        modelBodyParts["left_arm_upper"] = GameObjectUtilities.FindChildWithName(target, "Left_Arm_Upper");
+        modelBodyParts["right_arm_lower"] = GameObjectUtilities.FindChildWithName(target, "Right_Arm_Lower");
+        modelBodyParts["right_arm_upper"] = GameObjectUtilities.FindChildWithName(target, "Right_Arm_Upper");
+        modelBodyParts["head"] = GameObjectUtilities.FindChildWithName(target, "Head");
+    }
+
+    private void SetPastModelPose(AnimationStep animationStep)
+    {
+        SetPose(animationStep, _pastModelBodyParts);
+    }
+
+    private void SetCurrentModelPose(AnimationStep animationStep)
+    {
+        SetPose(animationStep, _currentModelBodyParts);
+    }
+
+    private void SetPose(AnimationStep animationStep, Dictionary<string, GameObject> model)
+    {
+        SetBodyPartRotation(model["body_lower"], animationStep.lowerBodyRotation);
+        SetBodyPartRotation(model["body_upper"], animationStep.upperBodyRotation);
+        
+        SetBodyPartRotation(model["left_leg_lower"], animationStep.lowerLeftLegRotation);
+        SetBodyPartRotation(model["left_leg_upper"], animationStep.upperLeftLegRotation);
+        
+        SetBodyPartRotation(model["right_leg_lower"], animationStep.lowerRightLegRotation);
+        SetBodyPartRotation(model["right_leg_upper"], animationStep.upperRightLegRotation);
+        
+        SetBodyPartRotation(model["left_arm_lower"], animationStep.lowerLeftArmRotation);
+        SetBodyPartRotation(model["left_arm_upper"], animationStep.upperLeftArmRotation);
+        
+        SetBodyPartRotation(model["right_arm_lower"], animationStep.lowerRightArmRotation);
+        SetBodyPartRotation(model["right_arm_upper"], animationStep.upperRightArmRotation);
+        
+        SetBodyPartRotation(model["head"], animationStep.headRotation);
+    }
+
+    private void SetBodyPartRotation(GameObject bodyPart, Vector3 initialPoseBodyRotation)
+    {
+        Quaternion quaternion = Quaternion.Euler(initialPoseBodyRotation);
+        bodyPart.transform.localRotation = quaternion;
+    }
+}
--- Proiect Licenta/Assets/Scripts/Service/AnimationEditor.cs
+++ Proiect Licenta/Assets/Scripts/Service/AnimationEditor.cs
@@ -5,70 +5,26 @@ using UnityEditor;
 using UnityEngine;
 using Utilities;
 
-public class AnimationEditor : MonoBehaviour
+public class AnimationEditor
 {
-    [SerializeField] private AnimationStep defaultPose;
-    [SerializeField] private GameObject currentPoseModel;
-    [SerializeField] private GameObject pastPoseModel;
-    [SerializeField] private Animation _animation;
-    [SerializeField] private string assetsPath = "Assets/TestStep/"; 
-    [SerializeField] private string animationName;
+    private AnimationStep defaultPose;
+    private Animation _animation;
+    private string assetsPath = "Assets/Animations/"; 
+    private string animationName;
     
     private int _index;
     private List<string> keys = new List<string>();
     private Dictionary<string, GameObject> _currentModelBodyParts = new Dictionary<string, GameObject>();
     private Dictionary<string, GameObject> _pastModelBodyParts = new Dictionary<string, GameObject>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        SetBodyParts(_currentModelBodyParts, currentPoseModel);
-        SetBodyParts(_pastModelBodyParts, pastPoseModel);
-        
-        _index = 0;
-        if (_animation.animationSteps.Count == 0)
-        {
-            Debug.Log("Not enough steps");
-            SetCurrentModelPose(defaultPose);
-            SetPastModelPose(defaultPose);
-        }
-        if (_animation.animationSteps.Count == 1)
-        {
-            Debug.Log("Not enough steps");
-            SetCurrentModelPose(_animation.animationSteps[_index]);
-            SetPastModelPose(_animation.animationSteps[_index]);
-        }
-        else
-        {
-            SetCurrentModelPose(_animation.animationSteps[_index]);
-            SetPastModelPose(_animation.animationSteps[_index + 1]);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
+    public AnimationEditor()
     {
         
-    }
-    
-    private void SetBodyParts(Dictionary<string, GameObject> modelBodyParts, GameObject target)
-    {
-        modelBodyParts["body_lower"] = GameObjectUtilities.FindChildWithName(target,"Body_Lower");
-        modelBodyParts["body_upper"] = GameObjectUtilities.FindChildWithName(target,"Body_Upper");
-        modelBodyParts["left_leg_lower"] = GameObjectUtilities.FindChildWithName(target,"Left_Leg_Lower");
-        modelBodyParts["left_leg_upper"] = GameObjectUtilities.FindChildWithName(target,"Left_Leg_Upper");
-        modelBodyParts["right_leg_lower"] = GameObjectUtilities.FindChildWithName(target,"Right_Leg_Lower");
-        modelBodyParts["right_leg_upper"] = GameObjectUtilities.FindChildWithName(target,"Right_Leg_Upper");
-        modelBodyParts["left_arm_lower"] = GameObjectUtilities.FindChildWithName(target,"Left_Arm_Lower");
-        modelBodyParts["left_arm_upper"] = GameObjectUtilities.FindChildWithName(target,"Left_Arm_Upper");
-        modelBodyParts["right_arm_lower"] = GameObjectUtilities.FindChildWithName(target,"Right_Arm_Lower");
-        modelBodyParts["right_arm_upper"] = GameObjectUtilities.FindChildWithName(target,"Right_Arm_Upper");
-        modelBodyParts["head"] = GameObjectUtilities.FindChildWithName(target,"Head");
     }
 
     public void CreateNewAnimation()
     {
-        
+        Debug.Log("New Animation Created");
     }
 
     public void SaveAnimation()
@@ -81,9 +37,10 @@ public class AnimationEditor : MonoBehaviour
         
     }
 
-    public void PreviewCurrentAnimation()
+    public void SetAnimation(Animation animation)
     {
-        
+        _animation = animation;
+        Debug.Log(_animation.animationSteps.Count);
     }
 
     public void AddNewAnimationStep()
@@ -93,44 +50,14 @@ public class AnimationEditor : MonoBehaviour
         AnimationStep newStep = ScriptableObject.CreateInstance<AnimationStep>();
         string assetName = String.Format("{0}_{1}.asset", animationName, _animation.animationSteps.Count);
         AssetDatabase.CreateAsset(newStep, assetsPath+assetName);
-        // TODO Save differently
         AssetDatabase.SaveAssets();
+        //TODO Export Animation as JSON too
         
         _animation.animationSteps.Add(newStep);
-
-        SetPastModelPose(previousStep);
-        SetCurrentModelPose(newStep);
     }
 
-    private void SetPastModelPose(AnimationStep animationStep)
+    public Animation GetCurrentAnimation()
     {
-        SetPose(animationStep, _pastModelBodyParts);
+        return _animation;
     }
-
-    private void SetCurrentModelPose(AnimationStep animationStep)
-    {
-        SetPose(animationStep, _currentModelBodyParts);
-    }
-
-    private void SetPose(AnimationStep animationStep, Dictionary<string, GameObject> model)
-    {
-        SetBodyPartRotation(model["body_lower"], animationStep.lowerBodyRotation);
-        SetBodyPartRotation(model["body_upper"], animationStep.upperBodyRotation);
-        SetBodyPartRotation(model["left_leg_lower"], animationStep.lowerLeftLegRotation);
-        SetBodyPartRotation(model["left_leg_upper"], animationStep.upperLeftLegRotation);
-        SetBodyPartRotation(model["right_leg_lower"], animationStep.lowerRightLegRotation);
-        SetBodyPartRotation(model["right_leg_upper"], animationStep.upperRightLegRotation);
-        SetBodyPartRotation(model["left_arm_lower"], animationStep.lowerLeftArmRotation);
-        SetBodyPartRotation(model["left_arm_upper"], animationStep.upperLeftArmRotation);
-        SetBodyPartRotation(model["right_arm_lower"], animationStep.lowerRightArmRotation);
-        SetBodyPartRotation(model["right_arm_upper"], animationStep.upperRightArmRotation);
-        SetBodyPartRotation(model["head"], animationStep.headRotation);
-    }
-    
-    private void SetBodyPartRotation(GameObject bodyPart, Vector3 initialPoseBodyRotation)
-    {
-        Quaternion quaternion = Quaternion.Euler(initialPoseBodyRotation);
-        bodyPart.transform.localRotation = quaternion;
-    }
-    
 }
