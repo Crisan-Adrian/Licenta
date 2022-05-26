4bc1288ddf9a91ba55b6f2e3b3d03d2be3f20c72
Crisan-Adrian <adriancatalincrisan@gmail.com>
2022-05-26 08:35:26 +0300

    Edit models package

--- Proiect_AI/models/__init__.py
+++ Proiect_AI/models/__init__.py
@@ -1,5 +1,6 @@
 from .iterator_model import create_iterator_model
 from .position_model import create_position_model
 from .primitive_model import create_primitive_model
+from .primitive_model import create_primitive_model2
 
-__all__ = ["create_iterator_model", "create_position_model", "create_primitive_model"]
+__all__ = ["create_iterator_model", "create_position_model", "create_primitive_model", "create_primitive_model2"]
