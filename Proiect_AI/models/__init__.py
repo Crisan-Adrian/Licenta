cc6f7fef334b6adbc64c3b3056775a9d859b57cb
Crisan-Adrian <adriancatalincrisan@gmail.com>
2022-05-27 22:05:20 +0300

    Ran experiment, no improvements.

--- Proiect_AI/models/__init__.py
+++ Proiect_AI/models/__init__.py
@@ -2,5 +2,7 @@ from .iterator_model import create_iterator_model
 from .position_model import create_position_model
 from .primitive_model import create_primitive_model
 from .primitive_model import create_primitive_model2
+from .primitive_model import create_primitive_model3
 
-__all__ = ["create_iterator_model", "create_position_model", "create_primitive_model", "create_primitive_model2"]
+__all__ = ["create_iterator_model", "create_position_model", "create_primitive_model", "create_primitive_model2",
+           "create_primitive_model3"]
